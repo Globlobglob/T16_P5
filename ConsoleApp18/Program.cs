0f91baf16b92f3cb341dd7c774e49337ac3bcc9d
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2024-02-02 14:04:45 +0200

    Add files via upload

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter n num");
+            int n = int.Parse(Console.ReadLine());
+
+            Worker[] omas = new Worker[n];
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine("Worker Name ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Worker Surname");
+                string surname = Console.ReadLine();
+                Console.WriteLine("Worker LastName");
+                string lastname = Console.ReadLine();
+                Console.WriteLine("Date year YYYY:MM:DD ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Post ");
+                string post = Console.ReadLine();
+                Console.WriteLine("Money ");
+                int money = int.Parse(Console.ReadLine());
+                    omas[i]= new Worker(name,surname,lastname, date, post, money);
+                Console.WriteLine();
+            }
+
+            foreach(var num in omas)
+            {
+                Worker w1 = new Worker(num.Name, num.Surname, num.Lastname, num.Date, num.Post, num.Money);
+                w1.Print();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp18/Worker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Worker
+    {
+         int tablenum;
+        public int Tablenum { get => tablenum; set => tablenum=value; }
+        /// <summary>
+        /// initialize surname field
+        /// </summary>
+        string surname;
+        public string Surname { get => surname; set =>surname=(value!="")?value:"Unknown"; }
+
+        /// <summary>
+        /// initialize name field
+        /// </summary>
+        string name;
+        public string Name { get => name;set=>name=(value!="")?value:"Unknown"; }
+
+        /// <summary>
+        /// initialize lastname fiiled
+        /// </summary>
+        string lastname;
+        public string Lastname { get => lastname; set => lastname = (value != "") ? value : "Unknown"; }
+        /// <summary>
+        /// init date filed DateTime
+        /// </summary>
+        private DateTime date;
+        public DateTime Date { get => date; set => date =value; }
+
+        /// <summary>
+        /// initialize string post 
+        /// </summary>
+        private string post;
+        public string Post { get => post; set => post = (value!="")?value:"Unknown"; }
+        /// <summary>
+        /// initialize int money field
+        /// </summary>
+        private int money;
+        public int Money { get => money; set => money = (value!=0)?value:0  ; }
+
+        /// <summary>
+        /// non static constructor with parameters
+        /// </summary>
+        /// <param name="surname"></param>
+        /// <param name="date"></param>
+        /// <param name="post"></param>
+        /// <param name="money"></param>
+        public Worker(string name,string surname, string lastname, DateTime date,string post,int money)
+        {
+            this.lastname = lastname;
+            this.name = name;
+            this.surname = surname;
+            this.date = date;
+            this.post = post;
+            this.money = money;
+            if (date.Month < DateTime.Now.Month) { Console.WriteLine( date.AddYears(-1)); }
+
+        }
+        /// <summary>
+        /// method print with output 
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($" \nName {name}\nSurname {surname}\n LastName {lastname}\nDate:{date:d}\nPost:{post}\nMoney:{money}\nAgeWorker:{DateTime.Now.Year-date.Year}");
+        }
+
+
+
+
+       
+    }
+}
